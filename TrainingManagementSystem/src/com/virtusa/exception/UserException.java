2e4f1c561194704107b815dfdf794c2697a07241
RAVI <RAVI@RAVI-PC>
2019-09-28 21:58:29 +0530

    commit

---
+++ TrainingManagementSystem/src/com/virtusa/exception/UserException.java
@@ -0,0 +1,14 @@
+package com.virtusa.exception;
+
+public class UserException extends RuntimeException {
+	private String message;
+	public UserException(String message) {
+		this.message=message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}
+
+
