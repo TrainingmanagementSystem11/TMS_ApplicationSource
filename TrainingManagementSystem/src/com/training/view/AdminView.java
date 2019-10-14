efed55c3243c2edc3ef9e980a2ed113655fa4897
ravurinaveen <ravurinaveen@HDRVELOCITY57.Virtusa.com>
2019-10-14 10:21:47 +0530

    commit

--- TrainingManagementSystem/src/com/training/view/AdminView.java
+++ TrainingManagementSystem/src/com/training/view/AdminView.java
@@ -1,31 +1,57 @@
 package com.training.view;
 
 import java.util.Scanner;
-
 public class AdminView {
-	public static final Scanner scanner = new Scanner(System.in);
-	public static void adminMenu()
-{
+
+	public static void mainAdminMenu()
+  {
 		System.out.println("*Welocome to Admin HomePage**");
 		System.out.println("====================================");
         System.out.println("1.View History");
         System.out.println("2.Schedule Training");
         System.out.println("3.Number Of Attendees");
         System.out.println("4.Add Venue");
-        }
-	     
-	     switch(ch) {
-	     case 1:
-	    	 System.out.println(viewHistory);
-	     case 2:
-	    	 System.out.println(ScheduleTraining);
-	     case 3:
-		     System.out.println(NumberOfAttendees);
-	     case 4:
-		     System.out.println(Add Venue);
-		  default
-		  System.out.println(GoToBackHome));
-	     }
-	
-	
+        try(Scanner scanner=new Scanner(System.in);){
+			System.out.print("\nOption:");
+			int option=scanner.nextInt();
+			
+			switch(option) {     
+			case 1:ViewHistory();
+		       break;
+		case 2:ScheduleTraining();
+			   break;
+		case 3:NumberOfAttendees();
+			   break;
+		case 4:AddVenue();
+			   break;
+		default:System.out.println("!ERROR[SELECT APPROPRIATE OPTION]");
+		mainAdminMenu();  
+     }
+     }catch(Exception e) {
+			
+			System.out.println("!ERROR[SELECT APPROPRIATE OPTION]");
+		}     
+    }
+
+	private static void AddVenue() {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+	private static void NumberOfAttendees() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void ScheduleTraining() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void ViewHistory() {
+		// TODO Auto-generated method stub
+		
+	} 
 }
+  
